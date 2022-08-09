c20683feb06d7fed29fd47840a9248697e97eefa
Brenda Miranda <mirandabrenda711@gmail.com>
2022-08-08 21:08:26 -0300

    Desafio08
    Condiciones, operadores y matemática vectorial

---
+++ Desafio07Camaras/Assets/Scrips/Enemy.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField]
+    [Range(1f, 10f)]
+    private float speed = 1f;
+
+    /*[SerializeField]
+    [Range(0, 1)]
+    private int typeEnemy = 0;*/
+    // Start is called before the first frame update
+    enum CyborgTypes {Stalker, Rioter }
+    [SerializeField] CyborgTypes cyborgType;
+    [SerializeField] Transform playerTransform;
+
+    private void Start()
+    {
+        
+    }
+    void Update()
+    {
+       /*if (tipeEnemy == 0)
+        {
+            Debug.Log(name + "-> SEGUIR AL JUGADOR");
+        }
+        if (tipeEnemy == 1)
+        {
+            Debug.Log(name + "-> ROTAR AL REDEDOR");
+        }*/
+        switch (cyborgType)
+        {
+            case CyborgTypes.Stalker:
+                ChasePlayer();
+                break;
+            case CyborgTypes.Rioter:
+                RotateAroundPlayer();
+                break;
+
+        }
+    }
+
+    // Update is called once per frame
+    void RotateAroundPlayer()
+    {
+        LookPlayer();
+        transform.RotateAround(playerTransform.position, Vector3.up, 5f * Time.deltaTime);
+    }
+    void ChasePlayer()
+    {
+        LookPlayer();
+        Vector3 direction = (playerTransform.position - transform.position);
+        if (direction.magnitude > 1f)
+        {
+            transform.position += direction.normalized * speed * Time.deltaTime;
+        }
+
+    }
+        private void LookPlayer()
+        {
+            transform.LookAt(playerTransform);
+        }
+        
+    
+}
--- Desafio07Camaras/Assets/Scrips/PlayerMovement.cs
+++ Desafio07Camaras/Assets/Scrips/PlayerMovement.cs
@@ -6,6 +6,7 @@ public class PlayerMovement : MonoBehaviour
 {
     public float speed = 3f;
     public GameObject munition;
+    public float cameraAxisX = 0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +16,7 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        RotatePlayer();
         if (Input.GetKey(KeyCode.W))
         {
             MovePlayer(Vector3.forward);
@@ -44,5 +46,12 @@ public class PlayerMovement : MonoBehaviour
     {
         Debug.Log("DISPARAR");
         Instantiate(munition, transform.position, transform.rotation);
+    }
+    public void RotatePlayer()
+    {
+        cameraAxisX += Input.GetAxis("Mouse X");
+        //transform.rotation = Quaternion.Euler(0, cameraAxisX * 0.5f, 0);
+        Quaternion newRotation = Quaternion.Euler(0, cameraAxisX, 0);
+        transform.rotation = Quaternion.Lerp(transform.rotation, newRotation, 2f * Time.deltaTime);
     }
 }
